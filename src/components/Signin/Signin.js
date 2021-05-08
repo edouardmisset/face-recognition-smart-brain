f945a8b6dd731ba2a1e03094b165bf31ff20bbd6
Edouard Misset <edouardmisset@gmail.com>
2021-05-09 00:16:52 +0200

    transformed singin component to a class

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,44 +1,63 @@
-import './Signin.css';
+import React, { Component } from 'react';
 
-export default function Signin() {
-  return (
-    <>
-      <form class='measure center' />
-      <fieldset id='sign_up' class='ba b--transparent ph0 mh0' />
-      <legend class='f4 fw6 ph0 mh0'>Sign In</legend>
-      <div class='mt3'>
-        <label class='db fw6 lh-copy f6' for='email-address'>
-          Email
-        </label>
-        <input
-          class='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
-          type='email'
-          name='email-address'
-          id='email-address'
-        />
-      </div>
-      <div class='mv3'>
-        <label class='db fw6 lh-copy f6' for='password'>
-          Password
-        </label>
-        <input
-          class='b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
-          type='password'
-          name='password'
-          id='password'
-        />
-      </div>
-        {/* </fieldset> */}
-        <div class=''>
-          <input
-            class='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib'
-            type='submit'
-            value='Sign in'
-          />
-        </div>
-        <div class='lh-copy mt3'>
-          <p class='f6 link dim black db'>Register</p>
-        </div>
-    </>
-  );
+export default class Signin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { onRouteChange } = this.props;
+    return (
+      <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
+        <main className='pa4 black-80'>
+          <div className='measure'>
+            <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
+              <legend className='f1 fw6 ph0 mh0'>Sign In</legend>
+              <div className='mt3'>
+                <label className='db fw6 lh-copy f6' htmlFor='email-address'>
+                  Email
+                </label>
+                <input
+                  className='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
+                  type='email'
+                  name='email-address'
+                  id='email-address'
+                  onChange={this.onEmailChange}
+                />
+              </div>
+              <div className='mv3'>
+                <label className='db fw6 lh-copy f6' htmlFor='password'>
+                  Password
+                </label>
+                <input
+                  className='b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
+                  type='password'
+                  name='password'
+                  id='password'
+                  onChange={this.onPasswordChange}
+                />
+              </div>
+            </fieldset>
+            <div className=''>
+              <input
+                onClick={this.onSubmitSignIn}
+                className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib'
+                type='submit'
+                value='Sign in'
+              />
+            </div>
+            <div className='lh-copy mt3'>
+              <p
+                onClick={() => onRouteChange('register')}
+                className='f6 link dim black db pointer'
+              >
+                Register
+              </p>
+            </div>
+          </div>
+        </main>
+      </article>
+    );
+  }
 }
