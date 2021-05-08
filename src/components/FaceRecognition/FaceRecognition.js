84d28e8e0a4173594711447dffc6aebe80673cbc
Edouard Misset <edouardmisset@gmail.com>
2021-05-08 20:32:28 +0200

    Added the bounding box

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,9 +1,21 @@
 import './FaceRecognition.css';
 
-export default function FaceRecognition({ imageUrl }) {
+export default function FaceRecognition({ imageUrl, box }) {
+  console.log(box);
   return (
     <div className='center mt4'>
-      <img src={imageUrl} alt='' />
+      <div className='mt2 absolute'>
+        <img src={imageUrl} alt='input' />
+        <div
+          className='bounding-box'
+          style={{
+            top: `${box.top_row * 100}%`,
+            right: `${box.right_col * 100}%`,
+            bottom: `${box.bottom_row * 100}%`,
+            left: `${box.left_col * 100}%`,
+          }}
+        ></div>
+      </div>
     </div>
   );
 }
