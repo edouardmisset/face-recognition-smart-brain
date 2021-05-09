3700ac5f664910e59ef6ef8c8cec6d206dafa62e
Edouard Misset <edouardmisset@gmail.com>
2021-05-09 12:26:30 +0200

    Reduced logo's margin

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import './Logo.css';
 export default function Logo() {
   return (
     <Tilt
-      className='parallax-effect ma4 super-center br-2 shadow-2'
+      className='parallax-effect super-center br-2 shadow-2'
       perspective={1000}
     >
       <div className='inner-element super-center'>🧠</div>
