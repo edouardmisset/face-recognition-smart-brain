53705902dea04fa092a0a1b4d3225efc1280ccfd
Edouard Misset <edouardmisset@gmail.com>
2021-05-09 12:21:22 +0200

    Logo adjustements

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import './Logo.css';
 export default function Logo() {
   return (
     <Tilt
-      className='parallax-effect ma4 mt0 super-center br-2 shadow-2'
+      className='parallax-effect ma4 super-center br-2 shadow-2'
       perspective={1000}
     >
       <div className='inner-element super-center'>🧠</div>
